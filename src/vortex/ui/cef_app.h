8953bb30a805781d0cffe2522fea4ad14246c9b1
Ilya Doroshenko <ilya.doroshenko@kdab.com>
2025-07-07 23:15:28 +0200

    2 way binding example

--- src/vortex/ui/cef_app.h
+++ src/vortex/ui/cef_app.h
@@ -4,8 +4,64 @@
 #include <include/cef_parser.h>
 #include <fstream>
 #include <vortex/util/log.h>
+#include <include/base/cef_bind.h>
 
 namespace vortex::ui {
+class Property
+{
+public:
+    Property(const std::string& initialValue = "")
+        : value_(initialValue) { }
+
+    std::string Get() const
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        return value_;
+    }
+
+    void Set(const std::string& newValue)
+    {
+        bool changed = false;
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            if (value_ != newValue) {
+                value_ = newValue;
+                changed = true;
+            }
+        }
+
+        if (changed && changeCallback_) {
+            changeCallback_(newValue);
+        }
+    }
+
+    void SetChangeCallback(std::function<void(const std::string&)> callback)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        changeCallback_ = callback;
+    }
+
+private:
+    std::string value_;
+    std::function<void(const std::string&)> changeCallback_;
+    mutable std::mutex mutex_;
+};
+
+// Global property for button text
+Property g_buttonText("Click Me!");
+
+struct VTask : public CefImplements<VTask, CefTask> {
+    VTask(std::function<void()> func)
+        : _func(std::move(func)) { }
+    void Execute() override
+    {
+        if (_func) {
+            _func();
+        }
+    }
+    std::function<void()> _func; ///< Function to execute when the task is run
+};
+
 class VortexResourceHandler : public CefImplements<VortexResourceHandler, CefResourceHandler>
 {
 public:
@@ -95,25 +151,106 @@ public:
                  CefString& exception) override
     {
         // Handle the JavaScript function call
-        if (name == "vortexCall") {
-            if (arguments.size() < 1) {
-                exception = "Invalid number of arguments";
-                return false;
+        if (name == "getButtonText") {
+            // Return the current button text
+            retval = CefV8Value::CreateString(g_buttonText.Get());
+            return true;
+        } else if (name == "setButtonText") {
+            // Set the button text from JavaScript
+            if (arguments.size() < 1 || !arguments[0]->IsString()) {
+                exception = "Expected string argument";
+                return true;
             }
-            // Process the function call here
-            // For example, you can log the arguments or perform some action
-            vortex::info("vortexCall executed with arguments: {}", arguments[0]->GetStringValue().ToString());
 
-            retval = CefV8Value::CreateString("Hello From C++!");
+            std::string newText = arguments[0]->GetStringValue();
+            g_buttonText.Set(newText);
+
+            retval = CefV8Value::CreateBool(true);
+            return true;
+        } else if (name == "registerButtonTextCallback") {
+            // Register a callback to be notified when button text changes
+            if (arguments.size() < 1 || !arguments[0]->IsFunction()) {
+                exception = "Expected function argument";
+                return true;
+            }
+
+            // Store the callback and context
+            buttonTextCallback_ = arguments[0];
+            callbackContext_ = CefV8Context::GetCurrentContext();
+
+            // Set up the C++ side callback
+            g_buttonText.SetChangeCallback([this](const std::string& newValue) {
+                NotifyJavaScript(newValue);
+            });
+
+            retval = CefV8Value::CreateBool(true);
             return true;
         }
-        exception = "Unknown function: " + name.ToString();
         return false; // Function not found
     }
 
+    // Notify JavaScript about property changes
+    void NotifyJavaScript(const std::string& newValue)
+    {
+        if (buttonTextCallback_.get() && callbackContext_.get() && callbackContext_->IsValid()) {
+            // Check if we're already on the correct thread
+            if (callbackContext_->IsSame(CefV8Context::GetCurrentContext())) {
+                // We're on the render thread with the right context, execute directly
+                return ExecuteCallback(newValue);
+            }
+        }
+        // Post to the render thread instead of UI thread
+        CefPostTask(TID_RENDERER, new VTask([this, value = newValue]() {
+                        if (callbackContext_.get() && callbackContext_->IsValid()) {
+                            // Enter the context and execute
+                            callbackContext_->Enter();
+                            ExecuteCallback(value);
+                            callbackContext_->Exit();
+                        }
+                    }));
+    }
+    void ExecuteCallback(const std::string& value)
+    {
+        if (buttonTextCallback_.get() && callbackContext_.get()) {
+            // Create arguments for the callback
+            CefV8ValueList args;
+            args.push_back(CefV8Value::CreateString(value));
+
+            // Call the JavaScript callback
+            CefRefPtr<CefV8Value> result;
+            CefRefPtr<CefV8Exception> exception;
+            buttonTextCallback_->ExecuteFunction(nullptr, args);
+
+            if (exception.get()) {
+                // Log the exception if there was an error
+                vortex::error("JavaScript callback error: {}", exception->GetMessage().ToString());
+            }
+        }
+    }
+
 private:
-    std::unordered_map<std::string, CefRefPtr<CefV8Value>> _registered_functions; ///< Map to store registered functions
+    CefRefPtr<CefV8Value> buttonTextCallback_;
+    CefRefPtr<CefV8Context> callbackContext_;
 };
+
+std::string GetCurrentTimeString()
+{
+    auto now = std::chrono::system_clock::now();
+    auto time = std::chrono::system_clock::to_time_t(now);
+    std::string timeStr = std::ctime(&time);
+    // Remove newline
+    if (!timeStr.empty() && timeStr[timeStr.length() - 1] == '\n') {
+        timeStr.erase(timeStr.length() - 1);
+    }
+    return timeStr;
+}
+
+// Function to simulate changing the button text from C++
+void SimulateButtonTextChange()
+{
+    // This could be called from another thread, timer, or event
+    g_buttonText.Set("Updated from C++ at " + GetCurrentTimeString());
+}
 
 class VortexCefApp : public CefImplements<VortexCefApp, CefApp, CefBrowserProcessHandler, CefRenderProcessHandler>
 {
@@ -141,8 +278,26 @@ public:
         // Register the JavaScript function
         CefRefPtr<CefV8Value> global = context->GetGlobal();
         CefRefPtr<VortexV8Handler> handler = new VortexV8Handler();
-        CefRefPtr<CefV8Value> func = CefV8Value::CreateFunction("vortexCall", handler);
-        global->SetValue("vortexCall", func, V8_PROPERTY_ATTRIBUTE_NONE);
+        // CefRefPtr<CefV8Value> func = CefV8Value::CreateFunction("vortexCall", handler);
+        global->SetValue("getButtonText",
+                         CefV8Value::CreateFunction("getButtonText", handler),
+                         V8_PROPERTY_ATTRIBUTE_NONE);
+
+        global->SetValue("setButtonText",
+                         CefV8Value::CreateFunction("setButtonText", handler),
+                         V8_PROPERTY_ATTRIBUTE_NONE);
+
+        global->SetValue("registerButtonTextCallback",
+                         CefV8Value::CreateFunction("registerButtonTextCallback", handler),
+                         V8_PROPERTY_ATTRIBUTE_NONE);
+
+        // Set up a timer to simulate C++ changes (optional)
+        std::thread([&]() {
+            while (true) {
+                std::this_thread::sleep_for(std::chrono::seconds(5));
+                SimulateButtonTextChange();
+            }
+        }).detach();
     }
 };
 } // namespace vortex::ui
